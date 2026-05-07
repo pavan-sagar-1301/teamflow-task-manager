be4e93ecada77b11783c3fe9c18d5a8a88360511
pavan-sagar-1301 <gg3903@srmist.edu.in>
2026-05-07 16:48:58 +0530

    Connecting frontend to live backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
-})
+  baseURL: "https://teamflow-task-manager-production-cc59.up.railway.app", 
+});
 
 api.interceptors.request.use((config) => {
 
