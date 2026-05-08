98e2424234f700814fe318ddcff58dd536545649
pavan-sagar-1301 <gg3903@srmist.edu.in>
2026-05-08 14:01:15 +0530

    Cleaned config and ensured tailwind plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,13 @@
 import { defineConfig } from 'vite'
-
 import react from '@vitejs/plugin-react'
-
 import tailwindcss from '@tailwindcss/vite'
 
-
-
 export default defineConfig({
-
-  plugins: [react()],
-
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   preview: {
-
     allowedHosts: ["teamflow-task-manager-production-03e2.up.railway.app"]
-
   }
-
 })
